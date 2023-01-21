6767d03284ed91c9356941c1bb28a7b2a50b503d
YeeshuPushparag <pushparagyeeshu@gmail.com>
2023-01-21 02:17:27 -0800

    finished project

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react'
-import BackgroundAnimation from '../components/BackgroundAnimation'
-import styles from '../styles/MainContainer.module.css'
+import React, { useEffect } from "react";
+import BackgroundAnimation from "../components/BackgroundAnimation";
+import styles from "../styles/MainContainer.module.css";
 const MainContainer = () => {
   useEffect(() => {
     const navbar = document.getElementById("navbar");
     const mainContainer = document.querySelector(`.${styles.mainContainer}`);
-    const observer = new IntersectionObserver((obj)=>{
-        obj.forEach((item)=>{
-         if (!item.isIntersecting) {
-          navbar.classList.add("fixed-nav")
-         }
-         else{
-          navbar.classList.remove("fixed-nav")
-         }
-        })
+    const observer = new IntersectionObserver((obj) => {
+      obj.forEach((item) => {
+        if (!item.isIntersecting) {
+          navbar.classList.add("fixed-nav");
+        } else {
+          navbar.classList.remove("fixed-nav");
+        }
+      });
     });
-    observer.observe(mainContainer)
-  }, [])
+    observer.observe(mainContainer);
+  }, []);
   return (
     <main className={styles.mainContainer}>
       <div className={styles.leftSection}>
@@ -25,15 +24,21 @@ const MainContainer = () => {
           My Personal Portfolio
         </h2>
         <p>
-       Hello everyone, My name is Yeeshu Pushparag and i am a web developer. I am experienced in frontend as well as in backend web development technologies.
+          Hello everyone, My name is Yeeshu Pushparag and i am a web developer.
+          I am experienced in frontend as well as in backend web development
+          technologies.
         </p>
-        <a href="#contact" className='primary-btn'><button><span>Have a Talk</span></button></a>
+        <a href="#contact" className="primary-btn">
+          <button>
+            <span>Have a Talk</span>
+          </button>
+        </a>
       </div>
       <div className={styles.rightSection}>
-        <BackgroundAnimation/>
+        <BackgroundAnimation />
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
